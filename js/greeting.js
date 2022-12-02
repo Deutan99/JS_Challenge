b94c277684919ba8305fe86f1008281c62273505
허재혁 <gjwogur0325@naver.com>
2022-12-02 17:30:56 +0900

    JS_Challenge 최종 과제

---
+++ js/greeting.js
@@ -0,0 +1,37 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+
+function handelLogoutClick() {
+  loginForm.classList.remove(HIDDEN_KEY);
+  home.classList.add(HIDDEN_KEY);
+  localStorage.removeItem(USERNAME_KEY);
+  localStorage.removeItem(TODOS_KET);
+}
+
+logoutButton.addEventListener("click", handelLogoutClick);
